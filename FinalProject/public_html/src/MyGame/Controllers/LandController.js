2b2a65bfd852cfc9a51271b82da6a34639be8a1b
SmirkinDino <344919926@qq.com>
2016-08-03 18:02:24 +0800

    sad

--- FinalProject/public_html/src/MyGame/Controllers/LandController.js
+++ FinalProject/public_html/src/MyGame/Controllers/LandController.js
@@ -25,25 +25,26 @@ function LandController(){
     //list of lands
     this.mUpLands = [];
     this.mDownLands = [];
-    this.kLandRes = "assets/land.png";
+    this.kLandResdown = "assets/landdown.png";
+    this.kLandResup = "assets/landup.png";
     
     this.kPerLandLength = 10;
-    this.kPerLandHeight = 4;
+    this.kPerLandHeight = 6;
     
-    this.kUpPosY = 7;
-    this.kDownPosY = -7;
+    this.kUpPosY = 8;
+    this.kDownPosY = -8;
     
     this.mMaxLandNum = 5;
     this.mLandSpeed = -0.2;
     
     
     for(var i = 0 ; i < this.mMaxLandNum; i++){
-        this.mUpLands[i] = new Way(new TextureRenderable(this.kLandRes));
+        this.mUpLands[i] = new Way(new TextureRenderable(this.kLandResup));
         this.mUpLands[i].getXform().setPosition(-gManager.DefaultOptions.FULL_SCREEN_WCWIDTH / 2 + this.kPerLandLength / 2 + this.kPerLandLength * i,this.kUpPosY);
         this.mUpLands[i].getXform().setSize(this.kPerLandLength,this.kPerLandHeight);
         gManager.ObjectPool.addObject(this.mUpLands[i],3);
         
-        this.mDownLands[i] = new Way(new TextureRenderable(this.kLandRes));
+        this.mDownLands[i] = new Way(new TextureRenderable(this.kLandResdown));
         this.mDownLands[i].getXform().setPosition(-gManager.DefaultOptions.FULL_SCREEN_WCWIDTH / 2 + this.kPerLandLength / 2 + this.kPerLandLength * i,this.kDownPosY);
         this.mDownLands[i].getXform().setSize(this.kPerLandLength,this.kPerLandHeight);
         gManager.ObjectPool.addObject(this.mDownLands[i],3);
